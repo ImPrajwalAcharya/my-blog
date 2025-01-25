d316ac3dec4d2ab59f7d59e34a6fa32d01e0bd9a
Prajwal-debug <prajwalsmart888@gmail.com>
2025-01-25 20:41:02 +0545

    Revert "changes"
    This reverts commit 496c26a6af6cd4da0bb522886efc6ea996b6ada0.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
-import React, { Suspense, lazy } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";  // Update import
 import Footer from "./components/Footer";
-import NoPage from "./components/NoPage";
-
-// Lazy load components for better performance
-const Home = lazy(() => import("./components/Home"));
-const PostDetail = lazy(() => import("./components/PostDetail"));
-const AllPostList = lazy(() => import("./components/AllPosts"));
-
-// Routes Configuration
-const ROUTES = {
-  HOME: "/",
-  BLOG_HOME: "/my-blog",
-  ALL_POSTS: "/my-blog/all-posts",
-  POST_DETAIL: "/post/:id",
-  NOT_FOUND: "*",
-};
+import Home from "./components/Home";
+import PostDetail from "./components/PostDetail";
+import AllPostList from "./components/AllPosts";
 
 function App() {
-  // Conditional Footer rendering based on route
-  const location = useLocation();
-  const shouldShowFooter = ![ROUTES.NOT_FOUND].includes(location.pathname);
-
   return (
-    <main className="min-h-screen bg-black text-white">
-      <Suspense fallback={<div className="text-center pt-10">Loading...</div>}>
-        <Routes>
-          <Route path={ROUTES.HOME} element={<Home />} />
-          <Route path={ROUTES.BLOG_HOME} element={<Home />} />
-          <Route path={ROUTES.ALL_POSTS} element={<AllPostList />} />
-          <Route path={ROUTES.POST_DETAIL} element={<PostDetail />} />
-          <Route path={ROUTES.NOT_FOUND} element={<NoPage />} />
+    <Router>
+      <main className="min-h-screen bg-black">
+        <Routes> {/* Replace Switch with Routes */}
+          <Route exact path="/" element={<Home />} /> {/* Update Route syntax */}
+          <Route path="/my-blog" element={<Home />} /> {/* Update Route syntax */}
+          <Route path="/my-blog/all-posts" element={<AllPostList />} /> {/* Update Route syntax */}
+          <Route path="/post/:id" element={<PostDetail />} />
         </Routes>
-      </Suspense>
-      {shouldShowFooter && <Footer />}
-    </main>
+        <Footer />
+      </main>
+
+    </Router>
   );
 }
 
--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -3,22 +3,16 @@ import { Link } from "react-router-dom";
 
 function PostCard({ post }) {
   return (
-    <div className="bg-custom-dark-blue bg-opacity-50 p-4 rounded-lg shadow-md flex flex-col h-full">
+    <div className="bg-custom-dark-blue p-4 rounded-lg shadow-md">
       <h2 className="text-white text-xl font-semibold">{post.title}</h2>
-      <img
-        src={post.image}
-        alt={post.title}
-        className="w-auto h-150 object-cover rounded-md mt-4"
-      />
-      <p className="text-subtle-gray mt-2 flex-grow line-clamp-5">{post.excerpt}</p>
-      <Link
-        to={`/post/${post.id}`}
-        className="text-link-color mt-4 self-start"
-      >
+      <img src={post.image} alt={post.title} className="w-auto h-150 object-cover rounded-md mt-4" />
+      <p className="text-subtle-gray mt-2">{post.excerpt}</p>
+      <Link to={`/post/${post.id}`} className="text-link-color mt-4 block">
         Read More
       </Link>
     </div>
   );
 }
+
 
 export default PostCard;
