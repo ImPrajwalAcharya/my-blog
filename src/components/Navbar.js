d8baaa4fd98fa502676bd94ab1bdd6cc56004e06
Prajwal-debug <prajwalsmart888@gmail.com>
2025-01-27 21:11:10 +0545

    logo added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,9 @@ function Navbar({ initialQuery }) {
     return (
         <nav className="fixed top-0 left-0 right-0 bg-black bg-opacity-50 text-white p-4 pl-6 pr-6 border-b border-gray-800">
             <div className="container mx-auto flex justify-between">
-                <h1 className="text-2xl font-bold">Blogs</h1>
+                <a href={ROUTES.HOME} className="text-2xl font-bold">
+                    <img src="logo192.png" alt="PA" className="inline-block h-12 w-12" />
+                </a>
                 <SearchBar
                     initialQuery={initialQuery}
                     onSearch={(query) => {
